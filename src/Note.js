15368417bc34f6ceabc41db9abf7d812742042af
Aaron <aaronjohndunphy@gmail.com>
2020-06-29 18:27:04 +0100

    Focus in textarea when adding a new note

--- src/Note.js
+++ src/Note.js
@@ -1,15 +1,31 @@
 import React from 'react'
 
-const Note = props => {
+class Note extends React.Component{
 
-    return (
-        <div className="note">
-            <textarea value={props.note.body} onChange={props.handleNoteUpdate}></textarea>
-            <div className="add-note">
-                <button onClick={props.handleAddNote}>+</button>
+    constructor(props){
+        super(props)
+        this.textarea = React.createRef();
+    }
+
+    componentDidUpdate(){
+        this.textarea.current.focus();
+    }
+
+    render(){
+        return (
+            <div className="note">
+                <textarea 
+                    value={this.props.note.body} 
+                    onChange={this.props.handleNoteUpdate} 
+                    ref={this.textarea}
+                >
+                </textarea>
+                <div className="add-note">
+                    <button onClick={this.props.handleAddNote}>+</button>
+                </div>
             </div>
-        </div>
-    );
+        )
+    }
 
 }
 
